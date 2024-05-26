87e56a88aec9b2ab83dfb17991fde5c713afddcc
Anna Zukowska <anna-zukowska@ya.ru>
2024-05-26 21:23:10 +0300

    commit hometask-3

--- src/test/java/com/example/teamcity/ui/SetupTest.java
+++ src/test/java/com/example/teamcity/ui/SetupTest.java
@@ -6,9 +6,11 @@ import com.example.teamcity.ui.pages.StartUpPage;
 import org.testng.annotations.Test;
 
 public class SetupTest extends BaseUiTest {
-
     @Test
     public void startUpTest() {
+        var testData = testDataStorage.addTestData();
+
+        loginAsUser(testData.getUser());
         new StartUpPage()
                 .open()
                 .setupTeamCityServer()
