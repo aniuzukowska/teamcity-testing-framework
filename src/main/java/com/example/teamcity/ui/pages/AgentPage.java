e590db037c02638e5be49314c727e33d7663238c
Anna Zukowska <anna-zukowska@ya.ru>
2024-05-28 23:56:57 +0300

    commit hometask-3

--- src/main/java/com/example/teamcity/ui/pages/AgentPage.java
+++ src/main/java/com/example/teamcity/ui/pages/AgentPage.java
@@ -8,10 +8,13 @@ import com.codeborne.selenide.Condition;
 import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.element;
+import static com.codeborne.selenide.Selenide.$x;
 
 public class AgentPage extends Page {
     private SelenideElement authButton = element(new ByAttribute("data-test-authorize-agent", "true"));
     private SelenideElement popup = element(Selectors.byDataTest("ring-popup"));
+    private SelenideElement submitButton = $x("//button[@type='submit' and .//span[text()='Authorize']]");
+
 
     public AgentPage open() {
         Selenide.open("/agents/unauthorized");
@@ -22,8 +25,8 @@ public class AgentPage extends Page {
     public AgentPage setupTeamCityAgent() {
         authButton.click();
         waitUntilPageIsLoaded();
-        popup.shouldBe(Condition.visible, Duration.ofSeconds(30));
-        submit();
+        popup.shouldBe(Condition.visible, Duration.ofSeconds(45));
+        submitButton.click();
         return this;
     }
 
